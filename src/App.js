3d85e0a11a70bc08c41f65218dab109946e55aed
Andy <ashishdeb1231@gmail.com>
2023-04-04 15:42:23 +0530

    Made showcase

--- src/App.js
+++ src/App.js
@@ -5,6 +5,12 @@ function App() {
 	return (
 		<>
 			<Navbar/>
+			<section className="scMain">
+				<div className="scTextWrapper">
+					<h1 className="scHead"> Order </h1>
+					<p className="scText"> Place your order, pay and relax while we bring your food. </p>
+				</div>
+			</section>
 		</>
 	);
 }
