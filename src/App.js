c4fc5f93188d96c40b8bbd5dd7db16de60281aeb
Andy <ashishdeb1231@gmail.com>
2023-04-05 10:36:08 +0530

    Making the search section

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import './App.css';
 import Navbar from './component/Navbar';
+import Arrow from './img/down arrow.svg'
+import Search from './img/search.svg'
+import Chicken from './img/chicken.png'
 
 function App() {
 	return (
@@ -10,6 +13,14 @@ function App() {
 					<h1 className="scHead"> Order </h1>
 					<p className="scText"> Place your order, pay and relax while we bring your food. </p>
 				</div>
+				<img src={Arrow} alt="" className="downArrow"/>
+			</section>
+			<section className="search">
+				<h1 className="searchHead"> Search your restaurant </h1>
+				<p className="searchText"> Just search and select. It’s that simple. </p>
+				<input type="text" className='searchBox'/>
+				<img src={Search} alt="" />
+				<img src={Chicken} alt="" />
 			</section>
 		</>
 	);
